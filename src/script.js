dba1c5bfb5d0bdf936de300aa279a6696be0c326
kennedybm <kennedy_muniz@hotmail.com>
2022-03-21 22:45:36 -0300

    Criado: formmulario para cadastro de usuario, modal de cadastro

---
+++ src/script.js
@@ -0,0 +1,16 @@
+import { Usuario } from "./models/cadastro.js";
+import { Modal } from "./models/modals.js";
+
+
+//pegando dados do form de cadastro
+const formCadastro = document.querySelector(".modal__cadastroForm");
+formCadastro.addEventListener("submit", Usuario.usuarioCadastrado);
+
+//modal de cadastro
+const botaoCadastro = document.getElementById("cadastro");
+botaoCadastro.addEventListener("click", Modal.modalCadastro);
+const botaoRemoverModal = document.querySelector(".modal__cadastroButton");
+botaoRemoverModal.addEventListener("click", Modal.modalCadastro);
+
+
+
