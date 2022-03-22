76ee3a302a7baa72a7ca66f7aeeb6d45a0a5b013
João Francisco Guarda <joaofrancisco.guarda@gmail.com>
2022-03-22 09:28:25 -0300

    Criado: Classe API com as rotas necessárias para todo o projeto

--- src/models/Api.js
+++ src/models/Api.js
@@ -1,8 +1,8 @@
 
 
 export class Api {
-  static async createUser(data) {
-    const response = await fetch(
+  static async criarUsuario(data) {
+    const resposta = await fetch(
       "https://kenzie-food-api.herokuapp.com/auth/register",
       {
         method: "POST", 
@@ -15,11 +15,11 @@ export class Api {
       .then((res) => res)
       .catch((error) => error);
 
-    return response;
+    return resposta;
   }
 
   static async login(data) {
-    const response = await fetch(
+    const resposta = await fetch(
       "https://kenzie-food-api.herokuapp.com/auth/login",
       {
         method: "POST", 
@@ -29,15 +29,11 @@ export class Api {
         body: JSON.stringify(data), 
       })
       .then((res) => res.json())
-      .then((response) => {
-
-        const token = response
+      .then((resposta) => {
+        const token = resposta
 
         window.localStorage.clear()
-
-        window.localStorage.setItem('token', response)
-        // window.localStorage.setItem('userId', userId)
-
+        window.localStorage.setItem('token', token)
       })
       .catch((error) => error);
   
@@ -45,17 +41,130 @@ export class Api {
   }
 
   static token = localStorage.getItem('token')
-  static userId = localStorage.getItem('userId')
 
-  static async getPublic() {
-    const response = await fetch(`https://kenzie-food-api.herokuapp.com/products`)
+  static async pegarProdutosPublico() {
+    const resposta = await fetch(`https://kenzie-food-api.herokuapp.com/products`)
       .then(res => res.json())
       .then(res => res)
 
-    return console.log(response)
+    return resposta
   }
 
+  static async pegarMeusProdutos () {
+    const resposta = await fetch(
+      "https://kenzie-food-api.herokuapp.com/my/products",
+      {
+        method: "GET", 
+        headers: {
+          "Content-Type": "application/json", 
+          "Authorization": `Bearer ${Api.token}` 
+        }
+      })
+      .then((res) => res.json())
+      .then((res) => res)
+      .catch((error) => error);
+    return resposta;
+  }
 
+  static async criarProduto(data) {
+    const resposta = await fetch(
+      "https://api-blog-m2.herokuapp.com/my/products",
+      {
+        method: "POST", 
+        headers: {
+          "Content-Type": "application/json", 
+          "Authorization": `Bearer ${Api.token}` 
+        },
+        body: JSON.stringify(data), 
+      })
+      .then((res) => res.json())
+      .then((res) => res)
+      .catch((error) => error);
+    return resposta;
+  }
 
+  static async atualizarProduto(data, idProduto) {
+    const resposta = await fetch(
+      `https://kenzie-food-api.herokuapp.com/my/products${idProduto}`,
+      {
+        method: "PATCH", 
+        headers: {
+          "Content-Type": "application/json", 
+          "Authorization": `Bearer ${Api.token}` 
+        },
+        body: JSON.stringify(data), 
+      })
+      .then((res) => res.json())
+      .then((res) => res)
+      .catch((error) => error);
+    return resposta;
+  }
+
+  static async deletarMeuProduto(idProduto) {
+    const resposta = await fetch(
+      `https://kenzie-food-api.herokuapp.com/my/products/:${idPost}`,
+      {
+        method: "DELETE", 
+        headers: {
+          "Content-Type": "application/json", 
+          "Authorization": `Bearer ${Api.token}` 
+        },
+        body: JSON.stringify(data), 
+      })
+      .then((res) => res.json())
+      .then((res) => res)
+      .catch((error) => error);
+    return resposta;
+  }
+
+  static async meuCarrinho () {
+      const resposta = await fetch(
+        "https://kenzie-food-api.herokuapp.com/cart",
+        {
+          method: "GET", 
+          headers: {
+            "Content-Type": "application/json", 
+            "Authorization": `Bearer ${Api.token}` 
+          }
+        })
+        .then((res) => res.json())
+        .then((res) => res)
+        .catch((error) => error);
+      return resposta;
+  }
+
+  static async colocarProdutoCarrinho (data) {
+    const resposta = await fetch(
+      "https://kenzie-food-api.herokuapp.com/cart",
+      {
+        method: "POST", 
+        headers: {
+          "Content-Type": "application/json", 
+          "Authorization": `Bearer ${Api.token}` 
+        },
+        body: JSON.stringify(data),
+      })
+      .then((res) => res.json())
+      .then((res) => res)
+      .catch((error) => error);
+    return resposta;
+  }
+
+  static async removerProdutoCarrinho(idProduto) {
+    const resposta = await fetch(
+      `https://kenzie-food-api.herokuapp.com/cart/remove/:${idProduto}`,
+      {
+        method: "DELETE", 
+        headers: {
+          "Content-Type": "application/json", 
+          "Authorization": `Bearer ${Api.token}` 
+        },
+        body: JSON.stringify(data), 
+      })
+      .then((res) => res.json())
+      .then((res) => res)
+      .catch((error) => error);
+    return resposta;
+  }
 
 }
